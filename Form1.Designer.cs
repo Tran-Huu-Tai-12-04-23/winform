376785fffafb63ad5e4eb4e83edaacb500e4544f
Khải Trương Trọng <52100966@student.tdtu.edu.vn>
2023-10-08 19:20:00 +0700

    Login

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            checkBox1 = new CheckBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.background;
+            pictureBox1.Location = new Point(0, 1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(631, 471);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.user;
+            pictureBox2.Location = new Point(630, 1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(469, 106);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlLightLight;
+            label1.ForeColor = SystemColors.AppWorkspace;
+            label1.Location = new Point(787, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Get Stared Form Login ";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.textbox1;
+            pictureBox3.Location = new Point(637, 133);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(462, 63);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.textbox1;
+            pictureBox4.Location = new Point(637, 202);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(462, 62);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlLight;
+            textBox1.ForeColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(803, 156);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(142, 27);
+            textBox1.TabIndex = 5;
+            textBox1.Text = "UserName";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ControlLight;
+            textBox2.ForeColor = SystemColors.ScrollBar;
+            textBox2.Location = new Point(802, 222);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(143, 27);
+            textBox2.TabIndex = 6;
+            textBox2.Text = "Phone Number";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.textbox1;
+            pictureBox5.Location = new Point(637, 270);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(462, 62);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 7;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.textbox1;
+            pictureBox6.Location = new Point(637, 338);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(462, 62);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 8;
+            pictureBox6.TabStop = false;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ControlLight;
+            textBox3.ForeColor = SystemColors.ScrollBar;
+            textBox3.Location = new Point(802, 294);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(143, 27);
+            textBox3.TabIndex = 9;
+            textBox3.Text = "Email";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.ControlLight;
+            textBox4.ForeColor = SystemColors.ScrollBar;
+            textBox4.Location = new Point(802, 359);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(143, 27);
+            textBox4.TabIndex = 10;
+            textBox4.Text = "Password";
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.phone;
+            pictureBox7.Location = new Point(803, 222);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(19, 27);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.letter;
+            pictureBox8.Location = new Point(803, 294);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(19, 27);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 12;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = Properties.Resources._lock;
+            pictureBox9.Location = new Point(802, 359);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(19, 27);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 13;
+            pictureBox9.TabStop = false;
+            pictureBox9.Click += pictureBox9_Click;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = Properties.Resources.user1;
+            pictureBox10.Location = new Point(803, 156);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(19, 27);
+            pictureBox10.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox10.TabIndex = 14;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Image = Properties.Resources.login1;
+            pictureBox11.Location = new Point(787, 406);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(181, 43);
+            pictureBox11.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox11.TabIndex = 15;
+            pictureBox11.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.ForeColor = SystemColors.ActiveCaption;
+            checkBox1.Location = new Point(630, 416);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(180, 24);
+            checkBox1.TabIndex = 16;
+            checkBox1.Text = "Get Stated Form Login";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(723, 452);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 20);
+            label2.TabIndex = 17;
+            label2.Text = "New User ?";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.OrangeRed;
+            label3.Location = new Point(817, 452);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 20);
+            label3.TabIndex = 18;
+            label3.Text = "Registred";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(886, 452);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 20);
+            label4.TabIndex = 19;
+            label4.Text = "Here";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1097, 471);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(checkBox1);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private CheckBox checkBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
